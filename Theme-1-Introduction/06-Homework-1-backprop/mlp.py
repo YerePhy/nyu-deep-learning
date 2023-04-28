a14ee96b05ae65b78b121ba30972e6472bbbc8b5
YerePhy <jereasap@gmail.com>
2023-04-28 17:25:44 +0200

    Adding material of the first Theme.

---
+++ Theme-1-Introduction/06-Homework-1-backprop/mlp.py
@@ -0,0 +1,103 @@
+import torch
+
+class MLP:
+    def __init__(
+        self,
+        linear_1_in_features,
+        linear_1_out_features,
+        f_function,
+        linear_2_in_features,
+        linear_2_out_features,
+        g_function
+    ):
+        """
+        Args:
+            linear_1_in_features: the in features of first linear layer
+            linear_1_out_features: the out features of first linear layer
+            linear_2_in_features: the in features of second linear layer
+            linear_2_out_features: the out features of second linear layer
+            f_function: string for the f function: relu | sigmoid | identity
+            g_function: string for the g function: relu | sigmoid | identity
+        """
+        self.f_function = f_function
+        self.g_function = g_function
+
+        self.parameters = dict(
+            W1 = torch.randn(linear_1_out_features, linear_1_in_features),
+            b1 = torch.randn(linear_1_out_features),
+            W2 = torch.randn(linear_2_out_features, linear_2_in_features),
+            b2 = torch.randn(linear_2_out_features),
+        )
+        self.grads = dict(
+            dJdW1 = torch.zeros(linear_1_out_features, linear_1_in_features),
+            dJdb1 = torch.zeros(linear_1_out_features),
+            dJdW2 = torch.zeros(linear_2_out_features, linear_2_in_features),
+            dJdb2 = torch.zeros(linear_2_out_features),
+        )
+
+        # put all the cache value you need in self.cache
+        self.cache = dict()
+
+    def forward(self, x):
+        """
+        Args:
+            x: tensor shape (batch_size, linear_1_in_features)
+        """
+        # TODO: Implement the forward function
+        pass
+    
+    def backward(self, dJdy_hat):
+        """
+        Args:
+            dJdy_hat: The gradient tensor of shape (batch_size, linear_2_out_features)
+        """
+        # TODO: Implement the backward function
+        pass
+
+    
+    def clear_grad_and_cache(self):
+        for grad in self.grads:
+            self.grads[grad].zero_()
+        self.cache = dict()
+
+def mse_loss(y, y_hat):
+    """
+    Args:
+        y: the label tensor (batch_size, linear_2_out_features)
+        y_hat: the prediction tensor (batch_size, linear_2_out_features)
+
+    Return:
+        J: scalar of loss
+        dJdy_hat: The gradient tensor of shape (batch_size, linear_2_out_features)
+    """
+    # TODO: Implement the mse loss
+    pass
+
+
+    # return loss, dJdy_hat
+
+def bce_loss(y, y_hat):
+    """
+    Args:
+        y_hat: the prediction tensor
+        y: the label tensor
+        
+    Return:
+        loss: scalar of loss
+        dJdy_hat: The gradient tensor of shape (batch_size, linear_2_out_features)
+    """
+    # TODO: Implement the bce loss
+    pass
+
+    # return loss, dJdy_hat
+
+
+
+
+
+
+
+
+
+
+
